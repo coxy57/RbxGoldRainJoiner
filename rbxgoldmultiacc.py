cbf600b91a934f689482a01bbfe812bd3392dac7
coxy57 <89717168+coxy57@users.noreply.github.com>
2023-09-03 23:19:45 -0400

    Update rbxgoldmultiacc.py

--- rbxgoldmultiacc.py
+++ rbxgoldmultiacc.py
@@ -10,13 +10,12 @@ import json, websocket, time, re, requests, threading, random
 # MADE BY COXY57 ON DISCORD
 # MADE BY COXY57 ON DISCORD
 
-# This is made for rbxgold.com to join the rains automatically 
+# This is made for rbxgold.com to join the rains automatically
 
 
 x = "MADE BY coxy57 ON DISCORD"
 y = "MADE BY coxy57 ON DISCORD"
 z = "MADE BY coxy57 ON DISCORD"
-
 
 # API KEY FOR CAPSOLVER.COM (you must create an account and get credit on the website then paste the key it gives you)
 APIKEY = ""
@@ -27,7 +26,7 @@ APIKEY = ""
 # Click on cookies, then go to rbxgold.com, then copy the value next to the SID in the name column
 # Paste it in between the "" in the SIDS [] first
 # if you need more than 3 sids, to add a new sid do ,"tokenhere"
-SIDS = ["","",""]
+SIDS = ["", "", ""]
 # the file you put the proxies in
 PROXY_FILE = "proxies.txt"
 # if you want to use proxies or not
@@ -35,15 +34,16 @@ USE_PROXIES = False
 
 
 class AutoJoinerHandler:
-    def __init__(self,apikey):
+    def __init__(self, apikey):
         self.apikey = apikey
-        self.website_key = "a3a5a9a9-7210-4dc7-a7bc-39c3fc73143e"   
+        self.website_key = "a3a5a9a9-7210-4dc7-a7bc-39c3fc73143e"
         self.user_agent = "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/116.0.0.0 Safari/537.36"
+
     def solve_captcha(self):
-        if not all(v in globals() for v in ("x","y","z")): return False
+        if not all(v in globals() for v in ("x", "y", "z")): return False
         if self.apikey is None:
             return False
-        captcha_created = requests.post('https://api.capsolver.com/createTask',json={
+        captcha_created = requests.post('https://api.capsolver.com/createTask', json={
             "clientKey": self.apikey,
             "task": {
                 "type": "HCaptchaTaskProxyLess",
@@ -56,11 +56,14 @@ class AutoJoinerHandler:
             return False
         taskid = captcha_created.json()['taskId']
         while True:
-            r = requests.post('https://api.capsolver.com/getTaskResult',json={'clientKey': self.apikey,'taskId': taskid}).json()
+            r = requests.post('https://api.capsolver.com/getTaskResult',
+                              json={'clientKey': self.apikey, 'taskId': taskid}).json()
             if r['status'] == "ready":
                 return r['solution']['gRecaptchaResponse']
-    
+
+
 auto_join = AutoJoinerHandler(APIKEY)
+
 
 class rblxGoldHandler(websocket.WebSocketApp):
     def __init__(self):
@@ -71,7 +74,8 @@ class rblxGoldHandler(websocket.WebSocketApp):
             on_error=self.on_error)
         self.rain_started = False
         self.proxies = open('proxies.txt').readlines() if USE_PROXIES else None
-    def handle_joiner(self,sid):
+
+    def handle_joiner(self, sid):
         if sid == "": return False
         captcha = auto_join.solve_captcha()
         if self.proxies:
@@ -82,24 +86,28 @@ class rblxGoldHandler(websocket.WebSocketApp):
         } if USE_PROXIES else None
         if captcha:
             join_game = requests.post('https://api.rbxgold.com/api/rain/rain-join',
-            params={
-                'hCaptchaToken': captcha
-            },
-            cookies={
-                        'SID': sid
-            },
-            proxies=rape_nigga)
+                                      params={
+                                          'hCaptchaToken': captcha
+                                      },
+                                      cookies={
+                                          'SID': sid
+                                      },
+                                      proxies=rape_nigga)
             if join_game.status_code == 200:
                 print('%s joined rain!' % sid)
             else:
-                print(join_game.text,join_game.status_code)
+                print(join_game.text, join_game.status_code)
         else:
             print('failed to solve or no api key')
-    def on_message(self,ws,message):
-        if not all(v in globals() for v in ("x","y","z")): return
+
+    def on_message(self, ws, message):
+        if not all(v in globals() for v in ("x", "y", "z")): return
+        message = str(message).strip()
+        if "2" == message:
+            ws.send("3")
         if 'rain-stream' in message:
             pattern = r'\[({.*})\]'
-            msg = json.loads(re.search(pattern,message).group(1)[:-2])
+            msg = json.loads(re.search(pattern, message).group(1)[:-2])
             if msg['status'] == "in progress" and self.rain_started == False:
                 get_rain_amt = int(msg['evAmount']) + msg['tipAmount']
                 # send to webhook if you want
@@ -108,17 +116,19 @@ class rblxGoldHandler(websocket.WebSocketApp):
                 # gets the rain amount
                 print('joining on %s accs' % len(SIDS))
                 for i in SIDS:
-                     threading.Thread(target=self.handle_joiner,args=(i,))
+                    threading.Thread(target=self.handle_joiner, args=(i,))
                 print('thread completion!')
             elif msg['status'] == "pending" and self.rain_started == True:
-                    self.rain_started = False
-                    print('rain ended')
+                self.rain_started = False
+                print('rain ended')
             else:
                 pass
-    def on_open(self,ws):
+
+    def on_open(self, ws):
         ws.send('40')
         time.sleep(1)
         ws.send('42["rain-join"]')
+
     def on_error(self, ws, error):
         pass
 
